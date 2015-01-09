45ce40681343e68e0961a813ff153447badba195
Rodrigo Silva <rcandidosilva@gmail.com>
2015-01-09 00:34:37 -0200

    Changes on bootstrap.sh

--- Vagrantfile
+++ Vagrantfile
@@ -11,7 +11,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   # Every Vagrant virtual environment requires a box to build off of.
 
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "chef/centos-6.5"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network :forwarded_port, host: 9000, guest: 9000
   config.ssh.forward_agent = true
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,54 +1,100 @@
 #!/bin/sh
-#sudo yum install wget -y
-#sudo yum install unzip -y
-#sudo yum install dos2unix -y
-#sudo yum install telnet -y
-#sudo yum install man -y
-#sudo yum install mysql-server -y
-#sudo /etc/init.d/mysqld start
+sudo yum -y install wget
+sudo yum -y install zip
+sudo yum -y install unzip
+sudo yum -y install dos2unix
+sudo yum -y install nano
+sudo yum -y install libyaml-devel
+sudo yum -y install zlib-devel
+sudo yum -y install curl-devel
+sudo yum -y install openssl-devel
+sudo yum -y install httpd-devel
+sudo yum -y install apr-devel
+sudo yum -y install apr-util-devel
+sudo yum -y install mysql-devel
+sudo yum -y install gcc
+sudo yum -y install ruby-devel
+sudo yum -y install gcc-c++
+sudo yum -y install make
+sudo yum -y install postgresql-devel
+sudo yum -y install ImageMagick-devel
+sudo yum -y install sqlite-devel
+sudo yum -y install perl-LDAP
+sudo yum -y install mod_perl
+sudo yum -y install perl-Digest-SHA
 
-#Set firewall rules
-#iptables -F
-#iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-#iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-#iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
-#iptables -P INPUT DROP
-#iptables -P FORWARD DROP
-#iptables -P OUTPUT ACCEPT
-#iptables -A INPUT -i lo -j ACCEPT
-#iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-#/sbin/service iptables save
-#iptables -L -v
+#Setup firewall rules
+iptables -F
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+/sbin/service iptables save
+iptables -L -v
 
-
-#cd /opt
+cd /opt
 
 #Download and install softwares
-#JDK
+
+#Install MySQL server
+sudo yum -y install mysql
+sudo yum -y install mysql-server
+sudo chkconfig mysqld on
+sudo service mysqld start
+
+#Install Apache
+yum -y install httpd
+chkconfig httpd on
+sudo service httpd start
+
+#Install Redmine
+#mysql --user=root --password=root_password_mysql
+#create database redmine_db character set utf8;
+#create user 'redmine_admin'@'localhost' identified by 'your_new_password';
+#grant all privileges on redmine_db.* to 'redmine_admin'@'localhost';
+#quit;
+
+#cd /var/www
+#wget http://www.redmine.org/releases/redmine-2.5.0.tar.gz
+
+#tar xvfz redmine-2.5.0.tar.gz
+#mv redmine-2.5.0 redmine
+#rm -rf redmine-2.5.0.tar.gz
+
+#cd /var/www/redmine/config
+#cp database.yml.example database.yml
+
+#Install JDK
 #sudo wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u55-b13/jdk-7u55-linux-x64.rpm
 #chmod +x jdk-7u55-linux-x64.rpm
 #rpm -ivh jdk-7u55-linux-x64.rpm
 
-#Install SonarCube
-#wget --progress=bar:force --no-cookies --no-check-certificate http://dist.sonar.codehaus.org/sonarqube-4.1.1.zip
-#unzip sonarqube-4.1.1.zip
-
-#sudo ln -s /opt/sonarqube-4.1.1/bin/linux-x86-64/sonar.sh /usr/bin/sonar
-#sudo cp /vagrant/replacement_files/sonar /etc/init.d/sonar
-#sudo chmod 755 /etc/init.d/sonar
-#sudo chkconfig --add sonar
-
 #Configure user .bash_profile
-#echo export JAVA_HOME=\"/usr/java/jdk1.7.0_13/\" >> $HOME/.bashrc
-#PATH=$JBOSS_HOME/bin:$PATH
+echo export JAVA_HOME=\"/usr/java/jdk1.7.0_55/\" >> $HOME/.bashrc
+PATH=$JAVA_HOME/bin:$PATH
+source $HOME/.bashrc
 
-#source $HOME/.bashrc
+#Install SonarCube
+wget --progress=bar:force --no-cookies --no-check-certificate http://dist.sonar.codehaus.org/sonarqube-4.5.2.zip
+unzip sonarqube-4.5.2.zip
 
-#sudo cp -f /vagrant/replacement_files/sonar.properties /opt/sonarqube-4.1.1/conf
-#sudo mysql < /vagrant/scripts/sonar.sql
+sudo ln -s /opt/sonarqube-4.5.2/bin/linux-x86-64/sonar.sh /usr/bin/sonar
+sudo cp /vagrant/replacement_files/sonar /etc/init.d/sonar
+sudo chmod 755 /etc/init.d/sonar
+sudo chkconfig --add sonar
+sudo cp -f /vagrant/replacement_files/sonar.properties /opt/sonarqube-4.5.2/conf
+sudo mysql < /vagrant/scripts/sonar.sql
+
+/etc/init.d/sonar start
 
 #Install Jenkins
-#sudo wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war
-#sudo java -jar jenkins.war &
+sudo wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war
+sudo java -jar jenkins.war &
 
-#/etc/init.d/sonar start
+#Install Nexus
+sudo wget http://download.sonatype.com/nexus/oss/nexus-2.11.1-01-bundle.zip
