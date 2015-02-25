0383ef91ef7d3ca26fd76f00ea42901e1f0dfd90
João Luiz Joaquim <joaoluizmj@gmail.com>
2015-02-25 19:45:21 -0300

    Delete bootstrap-mysql.sh

--- bootstrap-mysql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-iptables -I INPUT -p tcp --dport 3306 -j ACCEPT #MYSQL
-sudo yum -y install mysql-server
-sudo /sbin/service mysqld start
-sudo chkconfig mysqld on
-sudo /usr/bin/mysqladmin -u root password '123456'
